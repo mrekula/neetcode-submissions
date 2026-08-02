1ef750d363f2afb327de6f122abec9bbea0f92fc
mrekula <rekula88@gmail.com>
2026-08-02 13:14:53 -0400

    Add: lowest-common-ancestor-in-binary-search-tree - submission-3

---
+++ Data Structures & Algorithms/lowest-common-ancestor-in-binary-search-tree/submission-3.py
@@ -0,0 +1,51 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def lowestCommonAncestor(self, root: TreeNode, p: TreeNode, q: TreeNode) -> TreeNode:
+
+        # def dfs(root, p, q):
+
+        #     if not p or not q or not root:
+        #         return None
+
+        #     if max(p.val,q.val) < root.val:
+        #         return dfs(root.left, p, q)
+        #     elif min(p.val,q.val) > root.val:
+        #         return dfs(root.right, p, q)
+        #     else:
+        #         return root
+        # return dfs(root, p, q)
+
+
+        # curr = root
+
+        # while curr:
+        #     if max(p.val,q.val) < curr.val:
+        #         curr = curr.left
+        #     elif min(p.val, q.val) > curr.val:
+        #         curr = curr.right
+        #     else:
+        #         return curr
+
+        def dfs(root, p ,q):
+            if not root:
+                return None
+            if (p.val == root.val) or (q.val == root.val):
+                return root
+            left = dfs(root.left, p, q)
+            right = dfs(root.right, p, q)
+
+            if left and right:
+                return root
+            elif left:
+                return left
+            elif right:
+                return right
+        return dfs(root, p, q)
+        
+        
