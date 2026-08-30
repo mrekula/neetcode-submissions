100af8cceefc67000da770bc0641e0fdc2fbe748
mrekula <rekula88@gmail.com>
2026-08-30 18:49:28 -0400

    Add: meeting-schedule-ii - submission-3

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-3.py
@@ -0,0 +1,52 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        if len(intervals) == 0:
+            return 0
+        # time_stamps = []
+
+        # for interval in intervals:
+        #     time_stamps.append([interval.start, 1])
+        #     time_stamps.append([interval.end, -1])
+        # time_stamps.sort(key = lambda x: (x[0], x[1]))
+
+        # res = 0
+        # count = 0
+
+        # for i in time_stamps:
+        #     count += i[1]
+        #     res = max(res, count)
+        # return res
+
+        minheap = []
+        intervals.sort(key = lambda x : x.start)
+        for interval in intervals:
+            if minheap and minheap[0]  <= interval.start:
+                heapq.heappop(minheap)
+            heapq.heappush(minheap, interval.end)
+        print(minheap)
+        return len(minheap)
+
+
+
+
+
+
+
+
+
+
+
+
+        
+
+
+        
+        
